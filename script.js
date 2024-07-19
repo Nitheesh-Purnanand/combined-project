c2645a808f1c312d39204ae32b90f8ac777ca190
ch Nitheesh <chalumuri.nitheesh@gmail.com>
2024-07-20 00:56:57 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,9 @@
+fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>console.log(json));
+for(let i=0;i<=10;i++){
+    create();
+}
+function create(){
+    
+}
